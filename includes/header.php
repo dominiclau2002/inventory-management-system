de71a3dd691ddde113a73a85878468ed711f4f2b
Lau Tze Hao, Dominic <dominic.lau.2023@scis.smu.edu.sg>
2025-09-24 18:06:20 +0800

    Added favicon

--- includes/header.php
+++ includes/header.php
@@ -10,6 +10,7 @@ $is_logged_in = isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($page_title) ? $page_title . " - " : ""; ?><?php echo APP_SHORT_NAME; ?></title>
+    <link rel ="icon" type ="image/x-icon" href="../assets/images/favicon.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link href="<?php echo asset('css/style.css'); ?>" rel="stylesheet">
